5aec87616492b977add1644c788eba272286b873
kudoichika <kudoichika19@gmail.com>
2020-05-20 20:00:07 -0400

    Project Templates

--- Ultimate-ERS/AppDelegate.swift
+++ Ultimate-ERS/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Ultimate-ERS
 //
-//  Created by Kudo on 5/20/20.
+//  Created by kudoichika on 5/20/20.
 //  Copyright © 2020 kudoichika. All rights reserved.
 //
 
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        createUserDefaults()
+        initUserDefaults()
         return true
     }
 
