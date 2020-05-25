2099a761db4c8da4c874b2bc4409f95632187102
kudoichika <kudoichika19@gmail.com>
2020-05-25 17:50:44 -0400

    New requires for auth, session/cookies

--- server.js
+++ server.js
@@ -4,16 +4,30 @@ const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+//const flash = require('flash')
 require('./utils/config')
 
 const SERVER_PORT = process.env.ERS_PORT || 3000
 
-app.use('/', bodyParser.urlencoded({extended: true}))
-app.use('/', bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 require('./utils/database')(mongoose)
 require('./utils/socket')(io)
+require('./utils/passport')(passport)
 
-app.use('api/user', require('./api/users'))
+//app.use(flash())
+app.use(session({
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/api/users', require('./api/users'))
 
 server.listen(SERVER_PORT)
